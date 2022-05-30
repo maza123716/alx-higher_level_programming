5cbf45a64d97a2ce3d399848d1ffa1f1c35dbd66
maza123716 <mazafaith9919@gmail.com>
2022-05-30 15:12:51 -0700

    Create a new sentence

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+srt ="Python is an interpreted, interactive, object-oriented programming\language that combines remarkable ptax"
+str = str[39:66] + str[106:112] + str[0:6]
+print(str)
