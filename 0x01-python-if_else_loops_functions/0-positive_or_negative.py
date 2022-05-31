8095c15093a8c905252bf189410b96258dabf9fd
maza123716 <mazafaith9919@gmail.com>
2022-05-31 00:28:28 -0700

    Random signing

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randit(-10, 10)
+if number < 0:
+   print("{:d} is negative".format(number))
+elseif number >0:
+   print("{:d} is positive".format(number))
+else:
+   print("0 is zero)
