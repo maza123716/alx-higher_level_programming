cff718e7d9bb272b52b93023f5ca4fc7267cf70e
maza123716 <64093424+maza123716@users.noreply.github.com>
2022-06-02 17:36:23 +0300

    Infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-from sys import argv
-
 if __name__ == "__main__":
-     arg_num = len(argv) - 1
-     if arg_num == 0:
-          print("{}".format(arg_num))
-     else:
-          result = []
-          for i in range(1, arg_num + 1):
-              result.append(int(argv[i]))
-          print("{}".format(sum(result)))
+    from sys import argv
+    num_args = len(argv)
+    total = 0
+    for i in range(1, num_args):
+        total += int(argv[i])
+    print("{:d}".format(total))
