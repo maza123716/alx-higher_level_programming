7447030509b64bb896ae3bf93225eddce7c9bc54
maza123716 <64093424+maza123716@users.noreply.github.com>
2022-06-08 17:59:53 +0300

    PyBytesObject

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,6 +1,8 @@
 #include <Python.h>
+
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
+
 /**
  * print_python_list - Prints basic info about Python lists.
  * @p: A PyObject list object.
@@ -11,11 +13,14 @@ void print_python_list(PyObject *p)
         const char *type;
         PyListObject *list = (PyListObject *)p;
         PyVarObject *var = (PyVarObject *)p;
+
         size = var->ob_size;
         alloc = list->allocated;
+
         printf("[*] Python list info\n");
         printf("[*] Size of the Python List = %d\n", size);
         printf("[*] Allocated = %d\n", alloc);
+
         for (i = 0; i < size; i++)
         {
                 type = list->ob_item[i]->ob_type->tp_name;
@@ -24,6 +29,7 @@ void print_python_list(PyObject *p)
                         print_python_bytes(list->ob_item[i]);
         }
 }
+
 /**
  * print_python_bytes - Prints basic info about Python byte objects.
  * @p: A PyObject byte object.
@@ -32,18 +38,22 @@ void print_python_bytes(PyObject *p)
 {
         unsigned char i, size;
         PyBytesObject *bytes = (PyBytesObject *)p;
+
         printf("[.] bytes object info\n");
         if (strcmp(p->ob_type->tp_name, "bytes") != 0)
         {
                 printf("  [ERROR] Invalid Bytes Object\n");
                 return;
         }
+
         printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
         printf("  trying string: %s\n", bytes->ob_sval);
+
         if (((PyVarObject *)p)->ob_size > 10)
                 size = 10;
         else
                 size = ((PyVarObject *)p)->ob_size + 1;
+
         printf("  first %d bytes: ", size);
         for (i = 0; i < size; i++)
         {
