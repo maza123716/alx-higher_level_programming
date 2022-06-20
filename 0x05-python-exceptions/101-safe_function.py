9205db51bf5a4401e2f482dcad46c1121322c52a
maza123716 <64093424+maza123716@users.noreply.github.com>
2022-06-20 22:02:40 +0300

    Safe function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,21 +1,9 @@
 #!/usr/bin/python3
-
 import sys
-
-
-
-
-
 def safe_function(fct, *args):
-
     try:
-
         result = fct(*args)
-
     except Exception as ex:
-
         print("Exception: {}".format(ex), file=sys.stderr)
-
         return None
-
     return result
