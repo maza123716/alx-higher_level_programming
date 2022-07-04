789e19c781792516e42c1389e23836340f23b920
maza123716 <64093424+maza123716@users.noreply.github.com>
2022-07-04 22:17:22 +0300

    Full Rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,34 +1,37 @@
 #!/usr/bin/python3
 """
-This module implements a Rectangle object
+Contains definition of class Reactangle that inherits from BaseGeometry.
 """
-
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class Rectangle(BaseGeometry):
+class Rectangle(Basegeometry):
+    """Defnifition of class Rectangle that inherits from BaseGeometry.
+       Attributes:
+            width (int): width of the rectangle.
+            height (int) height of the rectangle.
+    """
+
     def __init__(self, width, height):
-        """initialisation
-        Args:
-            width (int): width
-            height (int): height
-        """
+        """Initializes an instance of class Rectangle"""
+
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+
         self.__width = width
+
         self.__height = height
 
     def area(self):
-        """area
-        Returns:
-            int: response
-        """
-        return self.__width * self.__height
+        """Returns are of the rectangle"""
+
+        area = self.__width * self.__height
+        return area
 
     def __str__(self):
-        """string
-        Returns:
-            str: string representation
         """
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+        Returns string representation of an instance of class rectangle
+        """
+        return "[{}] {}/{}".format(type(self).__name__,
+                                   self.__width, self.__height)
